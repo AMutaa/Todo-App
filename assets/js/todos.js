1f4998c18b32b1d2d3aa9cc0d2e85e3d81b9b6f0
AMutaa <piusandru@gmail.com>
2018-06-04 19:49:44 -0400

    updated css

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,27 +1,31 @@
 // Check Off Todos By Clicking
-$("ul").on("click", "li", function () {
- $(this).toggleClass("completed");
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("ul").on("click", "span", function (event) {
- $(this).parent().fadeOut(500, function () {
-  $(this).remove();
- });
- event.stopPropagation();
-})
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
 
 // Creating New Todos
-$("input[type='text']").keypress(function (event) {
- if (event.which === 13) {
-  var todoText = $(this).val(); /*grabbing new todo text from input*/
-  $(this).val(""); /*clearing the input*/
-  $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>"); /*create a new li and add to the ul*/
- }
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var todoText = $(this).val(); /*grabbing new todo text from input*/
+    $(this).val(""); /*clearing the input*/
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>"
+    ); /*create a new li and add to the ul*/
+  }
 });
 
 // the plus button
 
-$("#toggle-form").click(function () {
- $("input[type = 'text']").fadeToggle();
+$("#toggle-form").click(function() {
+  $("input[type = 'text']").fadeToggle();
 });
